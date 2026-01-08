e1596a24c91c8eead2e46a1d45c8b19866f622b5
sandpitturtle <->
2026-01-08 21:28:41 +0100

    added more tests and changed tests to send the data in chunks, as to mimic a tcp connection

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,44 +2,89 @@ package request
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"regexp"
 	"strings"
 )
 
+type state int
 const supportedVersion = "1.1"
+const crlf = "\r\n"
+
+const (
+    initialized state = iota
+    done 
+)
 
 type Request struct {
    RequestLine RequestLine 
+   state state
 }
 
 type RequestLine struct {
-    HttpVersion   string
-    RequestTarget string
     Method        string
+    RequestTarget string
+    HttpVersion   string
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-    requestString, err := io.ReadAll(reader)
-    if err != nil {
-       log.Fatal("request could not be slurped") 
+    parsedRequest := Request{}
+    line := make([]byte, 100)
+    window := make([]byte, 8)
+    var alreadyParsed int
+    var alreadyRead int
+//    var counter int
+    for {
+//        counter++
+        read, err := reader.Read(window)
+//        if counter >= 100 {
+//            break
+//        }
+//        fmt.Printf("read: %d \n", read)
+        for i:=alreadyParsed; i<alreadyRead + read; i++ {
+            if i > len(line) {
+                line = append(line, window[i - alreadyRead])
+            }
+            line[i] = window[i - alreadyRead]
+        }
+        alreadyRead += read
+        alreadyParsed += read
+        
+        _, err = parsedRequest.parse(line)
+
+
+        if err != nil {
+            return &Request{}, err
+        }
+        
+        if parsedRequest.state == done {
+            break
+        }
+        
     }
-    requestLine, err := parseRequestLine(string(requestString))
-    if err != nil {
-        return nil, err
-    }
-    return &Request{RequestLine: *requestLine},nil
+
+    return &parsedRequest, nil
 }
 
-func parseRequestLine(s string) (*RequestLine, error) {
-    split := strings.Split(s, "\r\n")
+func parseRequestLine(s string) (*RequestLine, int, error) {
+    split := strings.Split(s, crlf)
+    if len(split) < 2 {
+        //need more data to parse request line
+        return &RequestLine{}, 0, nil 
+    }
+    fmt.Println("===================")
+    fmt.Println("Gesamte Zeile:")
+    fmt.Println(s)
+    fmt.Println("===================")
+
     requestLine := split[0]
     requestLineSplit := strings.Split(requestLine, " ")
     if len(requestLineSplit) != 3 {
-        return &RequestLine{}, errors.New("request line doesn't have right amount of parts")
+        return &RequestLine{}, 0, errors.New("request line doesn't have right amount of parts")
     }
-    methodRegex, err := regexp.Compile("[A-Z]{3}")
+    methodRegex, err := regexp.Compile("[A-Z]{3,8}")
     if err != nil {
         log.Fatal("methodRegex couldn't be compiled")
     }
@@ -48,11 +93,11 @@ func parseRequestLine(s string) (*RequestLine, error) {
     version := requestLineSplit[2]
 
     if !methodRegex.Match([]byte(method)) {
-        return &RequestLine{}, errors.New("method does contain illegal characters")
+        return &RequestLine{}, 0, errors.New("method does contain illegal characters")
     }
 
     if !strings.Contains(version, supportedVersion) || len(version) < 5 {
-        return &RequestLine{}, errors.New("version not supported or to tiny")
+        return &RequestLine{}, 0, errors.New("version not supported or to tiny")
     }
     version = version[5:]
 
@@ -61,6 +106,40 @@ func parseRequestLine(s string) (*RequestLine, error) {
         RequestTarget: target,
         Method: method,
     }
-    
-    return &out, nil
+
+    fmt.Println("===================")
+    fmt.Println("Request Struc:")
+    fmt.Println(out)
+    fmt.Println("===================")
+    return &out, len(s), nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+    switch r.state {
+    case initialized:
+        s := string(data)
+        parsedRequest, consumed, err := parseRequestLine(s)
+
+        if err != nil {
+            // maybe?
+            fmt.Println("error in parseRequest Line")
+            fmt.Println(err)
+            r.state = done
+            return 0, err
+        }
+
+        if consumed != 0 {
+            r.RequestLine.Method = parsedRequest.Method
+            r.RequestLine.RequestTarget = parsedRequest.RequestTarget
+            r.RequestLine.HttpVersion = parsedRequest.HttpVersion
+            r.state = done
+        }
+        return len(data), nil
+    case done:
+        return 0, errors.New("trying to read into request with state done")
+    default:
+        return 0, errors.New("unsupported state")
+    }
+
+
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"fmt"
+	"io"
 	"strings"
 	"testing"
 
@@ -8,27 +10,62 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
 
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call
+// its useful for simulating reading a variable number of bytes per chunk from a network connection
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
+}
 
 func TestRequestLineParse(t *testing.T) {
+    reader := &chunkReader{
+        data: "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+        numBytesPerRead: 3,
+    }
+
     // Happy GET-Request | RequestLine
-    r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    r, err := RequestFromReader(reader)
     require.NoError(t, err)
     require.NotNil(t, r)
     assert.Equal(t, "GET", r.RequestLine.Method)
     assert.Equal(t, "/", r.RequestLine.RequestTarget)
     assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
+    fmt.Println(r)
+
+    reader = &chunkReader{
+        data: "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+        numBytesPerRead: 1,
+    }
     // Happy GET-Request | RequestLine with path
-    r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    r, err = RequestFromReader(reader)
     require.NoError(t, err)
     require.NotNil(t, r)
     assert.Equal(t, "GET", r.RequestLine.Method)
     assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
     assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 
+    reader = &chunkReader{
+        data: "POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+        numBytesPerRead: len(reader.data),
+    }
     // Happy POST-Request | RequestLine with path
-    r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+    r, err = RequestFromReader(reader)
     require.NoError(t, err)
     require.NotNil(t, r)
     assert.Equal(t, "POST", r.RequestLine.Method)
